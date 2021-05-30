f8999df55eeb87d1c562bffe3d331799e690029e
Stagnet <brucewayne128gb@protonmail.com>
2021-05-30 17:43:41 +0530

    update

--- js/main.js
+++ js/main.js
@@ -312,11 +312,7 @@ AOS.init({
     closeBtnInside: false,
     fixedContentPos: true,
     mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
-    },
+
     image: {
       verticalFit: true,
     },
